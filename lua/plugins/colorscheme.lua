a6b590e134307a86bf387ca3e534b506f37cb5c3
hrantgartley <doyouevenjump@gmail.com>
2023-11-20 16:58:52 -0600

    disabled lazy loading for colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -9,10 +9,15 @@ return {
   },
   {
     "craftzdog/solarized-osaka.nvim",
-    lazy = true,
+    lazy = false,
     priority = 1000,
     opts = function()
       transparent = true
     end,
+  },
+  {
+    "lunarvim/synthwave84.nvim",
+    lazy = false,
+    priority = 1000,
   },
 }
