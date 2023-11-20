026ce804704b83a6f5d3cb58aad71095f10ad009
hrantgartley <doyouevenjump@gmail.com>
2023-11-19 21:12:32 -0600

    stuff

--- init.lua
+++ init.lua
@@ -1,13 +1,4 @@
 require("config.lazy")
-vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
-vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
-vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
-vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
-vim.wo.relativenumber = true
-vim.wo.number = true
-vim.opt.swapfile = false
-vim.wo.cursorline = false
-vim.o.scrolloff = 2
 -- require("notify").setup({
 --   background_colour = "#FFFFFF",
 -- })
@@ -33,6 +24,7 @@ require("lspconfig").clangd.setup({
   cmd = {
     "clangd",
     "--offset-encoding=utf-16",
+    "--query-driver=/usr/local/bin/g++-13",
   },
   lazy = true,
 })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -10,3 +10,13 @@ vim.cmd([[
 ]])
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
 vim.cmd([[let &t_Ce = "\e[4:0m"]])
+vim.opt.mouse = ""
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+vim.wo.relativenumber = true
+vim.wo.number = true
+vim.opt.swapfile = false
+vim.wo.cursorline = false
+vim.o.scrolloff = 2
