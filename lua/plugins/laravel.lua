185f9400639d24608716fb496747e681b90bb2c7
hrantgartley <doyouevenjump@gmail.com>
2023-12-07 18:38:39 -0600

    add laravel stuff

---
+++ lua/plugins/laravel.lua
@@ -0,0 +1,27 @@
+return {
+    "adalessa/laravel.nvim",
+    dependencies = {
+        "nvim-telescope/telescope.nvim",
+        "tpope/vim-dotenv",
+        "MunifTanjim/nui.nvim",
+    },
+    cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
+    keys = {
+        { "<leader>aa", ":Laravel artisan<cr>" },
+        { "<leader>ar", ":Laravel routes<cr>" },
+        { "<leader>am", ":Laravel related<cr>" },
+        {
+            "<leader>at",
+            function()
+                require("laravel.tinker").send_to_tinker()
+            end,
+            mode = "v",
+            desc = "Laravel Application Routes",
+        },
+    },
+    event = { "VeryLazy" },
+    config = function()
+        require("laravel").setup()
+        require("telescope").load_extension "laravel"
+    end,
+}
