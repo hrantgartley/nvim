fc31799a48b544afa1cf48bd442b6d0aa8211122
hrantgartley <doyouevenjump@gmail.com>
2023-12-30 12:48:15 -0600

    fixed colorscheme delay

--- init.lua
+++ init.lua
@@ -1,16 +1,4 @@
 require("config.lazy")
--- require("notify").setup({
---   background_colour = "#FFFFFF",
--- })
-
--- require("tokyonight").setup({
---   transparent = true,
---   styles = {
---     sidebars = "transparent",
---     floats = "transparent",
---   },
---   lazy = true,
--- })
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
   update_in_insert = true,
@@ -49,25 +37,7 @@ timer:start(
     set_color_scheme()
   end)
 )
-
---[[ require("neo-tree").setup({
-  filesystem = {
-    window = {
-      mappings = {
-        ["<leader>p"] = "image_wezterm", -- " or another map
-      },
-    },
-    commands = {
-      image_wezterm = function(state)
-        local node = state.tree:get_node()
-        if node.type == "file" then
-          require("image_preview").PreviewImage(node.path)
-        end
-      end,
-    },
-    lazy = true,
-  },
-}) ]]
+set_color_scheme()
 
 local rt = require("rust-tools")
 rt.setup({
@@ -79,34 +49,6 @@ rt.setup({
   lazy = true,
 })
 
--- add clippy to rust-analyzer
--- local lspconfig = require("lspconfig")
--- lspconfig.rust_analyzer.setup({
---   -- add clippy to rust-analyzer
---   settings = {
---     ["rust-analyzer"] = {
---       assist = {
---         importEnforceGranularity = true,
---         importPrefix = "create",
---       },
---       cargo = { allFeatures = true },
---       checkOnSave = {
---         -- default: `cargo check`
---         command = "clippy",
---         allFeatures = true,
---       },
---       inlayHints = {
---
---         lifetimeElisionHints = {
---           enable = true,
---           useParameterNames = true,
---         },
---       },
---     },
---   },
--- })
-
---
 require("lspconfig").intelephense.setup({
   settings = {
     intelephense = {
@@ -125,22 +67,13 @@ require("lspconfig").html.setup({
   filetypes = { "html", "php" },
   lazy = true,
 })
---
---
+
 require("luasnip.loaders.from_vscode").lazy_load({ paths = "~/.config/nvim/snippets" })
 
 local on_attach = function(client, bufnr)
-  -- Disable hover in favor of Pyright
   client.server_capabilities.hoverProvider = false
 end
-
-require("lspconfig").ruff_lsp.setup({
-  on_attach = on_attach,
-  lazy = true,
-})
 
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 local cmp = require("cmp")
 cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-
-vim.g.root_spec = { "lsp", { ".git", "lua" }, "cwd" }
