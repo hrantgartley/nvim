98202be199cc7d809b047a434275550b5e69eace
hrantgartley <doyouevenjump@gmail.com>
2024-01-11 16:43:24 -0600

    block cursor

--- init.lua
+++ init.lua
@@ -50,3 +50,5 @@ end
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 local cmp = require("cmp")
 cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+
+vim.opt.guicursor = ""
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,12 +6,14 @@
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
 vim.cmd([[let &t_Ce = "\e[4:0m"]])
 -- editor options
-vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.wo.relativenumber = true
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.opt.swapfile = false
+vim.wo.number = true
+vim.wo.cursorline = false
 vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
-vim.wo.relativenumber = true
-vim.wo.number = true
-vim.opt.swapfile = false
-vim.wo.cursorline = false
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
 vim.o.scrolloff = 2
+-- vim.o.colorcolumn = "110"
+vim.o.guicursor = ""
