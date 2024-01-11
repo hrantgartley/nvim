567c6563656c753c1065905294d2bd97f2a58ce0
hrantgartley <doyouevenjump@gmail.com>
2024-01-10 21:12:12 -0600

    added keymap similar to mini.move

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -90,3 +90,6 @@ end, { desc = "Lazygit (cwd)" })
 -- vim.api.nvim_set_keymap(
 --  "n",--  "<leader>cc",
 --
+-- move lines (idk how to explain try it)
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
