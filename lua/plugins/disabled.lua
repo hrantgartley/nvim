543be9baa5978aba2604ea692f6b38d4adae2856
hrantgartley <doyouevenjump@gmail.com>
2024-01-18 13:01:51 -0600

    nu

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,7 +2,7 @@ return {
   { "folke/flash.nvim", enabled = false },
   { "ellisonleao/gruvbox.nvim", enabled = false },
   { "nvimdev/dashboard-nvim", enabled = false },
-  { "RRethy/vim-illuminate", enabled = false },
+  -- { "RRethy/vim-illuminate", enabled = false },
   { "echasnovski/mini.surround", enabled = fale },
   -- { "nvim-treesitter/nvim-treesitter-context", enabled = false },
 }
